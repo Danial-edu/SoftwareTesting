6e29efdf721d72628e8804110036fc36c9e82cb2
Weaam <weaamasad9@gmail.com>
2025-11-02 10:06:24 +0200

    A new commentss

--- ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/src/ThreadCheckArray.java
@@ -4,8 +4,8 @@ public class ThreadCheckArray implements Runnable
 	private boolean [] winArray;
 	SharedData sd;
 	int[] array;
-	int b;
-	
+	int b,c;
+	//just a comment
 	public ThreadCheckArray(SharedData sd) 
 	{
 		this.sd = sd;	
